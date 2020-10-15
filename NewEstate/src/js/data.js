0aa6066298697fc1be7482733bb72ea9b7f0d64e
Doreen162 <doreenligman17@gmail.com>
2020-10-15 16:32:49 +0200

    changes

--- NewEstate/src/js/data.js
+++ NewEstate/src/js/data.js
@@ -1,67 +1,75 @@
 
 let places = [
   {
-    image: "../../public/images/Estate.jpg",
+    image: "https://capeholidays.info/files/Large-Apartment-To-Rent-In-Cape-Town-City-Bowl14.jpg",
     type: "Apartment",
     location: "Paarl",
     price: 1000,
-    bedrooms: 1,
+    bedrooms: "1",
     status: "For rent",
   },
   {
-    image: "",
+    image: "https://cdn.remax.co.za/listings/2747199/original/pic_34823019.jpg",
     type: "Estate House",
     location: "Constantia",
     price: 2500,
-    bedrooms: 2,
+    bedrooms: "1",
     status: "For sale",
   },
   {
-    image: "",
+    image: "https://www.rentcafe.com/blog/wp-content/uploads/sites/5/2018/07/residential-building_890x400.png",
     type: "Condo",
     location: "Rondebosch",
     price: 1500,
-    bedrooms: 3,
+    bedrooms: "1",
     status: "For rent",
   },
   {
-    image: "",
+    image: "http://www.villavdl.co.uk/user/cimage/Villa.png",
     type: "Villa",
     location: "Camps Bay",
     price: 5000,
-    bedrooms: 3,
+    bedrooms: "1",
     status: "For sale",
   },
   {
-    image: "",
+    image: "https://www.cometocapetown.com/wp-content/uploads/2018/04/Top-10-Luxury-Apartments-in-Cape-Town-for-the-Perfect-Staycation-Artea.jpg",
     type: "Apartment",
     location: "Paarl",
     price: 3500,
-    bedrooms: 2,
+    bedrooms: "1",
     status: "For sale",
   },
   {
-    image: "",
+    image: "https://www.edenrockestate.co.za/wp-content/uploads/2016/02/Eden-Rock-1480-View-1-Final-High-Resolution.jpg",
     type: "Estate House",
     location: "Constantia",
     price: 4000,
-    bedrooms: 3,
+    bedrooms: "1",
     status: "For rent",
   },
   {
-    image: "",
+    image: "https://images.adsttc.com/media/images/51d6/fde6/e8e4/4ebb/5000/001b/large_jpg/VIEW_08_-_PENTHOUSE_TERRACE_VIEW_LOOKING_NORTH.jpg?1373044156",
     type: "Condo",
     location: "Rondebosch",
     price: 1000,
-    bedrooms: 2,
+    bedrooms: "1",
     status: "For sale",
   },
   {
-    image: "",
+    image: "https://southafricatoday.net/wp-content/uploads/2019/06/growth-of-real-estate.jpg",
     type: "Villa",
     location: "Camps Bay",
     price: 4500,
-    bedrooms: 1,
+    bedrooms: "1",
+    status: "For rent",
+  },
+  {
+    image: "https://southafricatoday.net/wp-content/uploads/2019/06/growth-of-real-estate.jpg",
+    type: "Villa",
+    location: "Camps Bay",
+    price: 4500,
+    bedrooms: "1",
     status: "For rent",
   },
 ];
@@ -74,25 +82,32 @@ function search() {
   let searchBed = document.getElementById("bedrooms").value;
   let searchStatus = document.getElementById("status").value;
 
+  console.log(searchType)
+  console.log(searchLocation)
+  console.log(searchBed)
+  console.log(searchStatus)
+
   // filter
   var myPlaces = places.filter((e) => {
-    if (
+   /* if (
       e.type == searchType &&
       e.location == searchLocation &&
       e.status == searchStatus &&
       e.bedrooms == searchBed
     ) {
       return e;
-    }
+    }*/
+    return e;
   });
 
   console.log(myPlaces);
   // the card
   let card = "";
   for (let i = 0; i < myPlaces.length; i++) {
-    card += `<div class="card">
+    card += `
+    <div class="card">
         <div class="card-header">
-        <img class="card-img" src=${myPlaces[i].image} alt="property-img">
+        <img class="card-img" src=${myPlaces[i].image}>
         </div>
         <div class="card-content">
             <h3>${myPlaces[i].type}</h3>
@@ -106,9 +121,7 @@ function search() {
         <div class="card-footer">
             <ul>
                 <li>
-                <svg style="width:26px;height:26px" viewBox="0 0 24 13">
-                    <path fill="#c7cace" d="M19,7H5V14H3V5H1V20H3V17H21V20H23V11A4,4 0 0,0 19,7" />
-                    </svg> 
+                
                     ${myPlaces[i].bedrooms} Bedrooms
                 </li>
                 
@@ -120,5 +133,3 @@ function search() {
   }
   document.getElementById("properties-slider").innerHTML = card;
 };
-
-search();
